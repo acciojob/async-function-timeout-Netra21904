9cab1505092500d9f0b37c464e7ca284b8032aaa
Netra21904 <128806086+Netra21904@users.noreply.github.com>
2025-03-06 12:49:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+let output = document.getElementById("output");
+console.log(output);
+let text = document.getElementById("text").value();
+console.log(text);
+let delay = document.getElementById("delay").value();
+console.log(delay);
+
+document.getElementById("btn").addEventListener("click", async () => {
+	let p = new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve();
+		}, delay)
+	});
+
+	let res = await p;
+	output.innerText = text;
+	console.log("innerText", output.innerText);
+});
