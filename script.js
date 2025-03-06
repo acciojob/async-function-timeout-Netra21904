2a77b7ab3fe861e29e8f07bbbeead407620b521d
Netra21904 <128806086+Netra21904@users.noreply.github.com>
2025-03-06 13:01:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-const outputDiv = document.getElementById("output");
+//your JS code here. If required.
 const textInput = document.getElementById("text");
 const delayInput = document.getElementById("delay");
+const outputDiv = document.getElementById("output");
 const button = document.getElementById("btn");
 
-button.addEventListener("click", async () => {
+button.addEventListener("click", async()=>{
 	const text = textInput.value;
 	const delay = parseInt(delayInput.value);
 	outputDiv.innerHTML = "";
-	await new Promise((resolve => setTimeout(resove, delay)); 
+	await new Promise(resolve => setTimeout(resolve,delay));
 	outputDiv.innerText = text;
-);		
+});
